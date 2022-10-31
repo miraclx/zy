9b3db5f7ed8f2a0f6a653e63444a1d22f9a52c3c
Miraculous Owonubi <omiraculous@gmail.com>
2022-10-31 16:09:16 +0400

    human readable cache time parsing

--- src/cli.rs
+++ src/cli.rs
@@ -2,9 +2,11 @@ use std::env;
 use std::ffi::OsStr;
 use std::io;
 use std::net::{AddrParseError, IpAddr, SocketAddr};
+use std::num::{FpCategory, IntErrorKind};
 use std::path::PathBuf;
 
 use clap::{AppSettings, Parser};
+use humantime::{parse_duration, DurationError};
 
 pub const DEFAULT_PORT: u16 = 3000;
 
@@ -32,6 +34,42 @@ fn parse_canonicalize_dir(s: &OsStr) -> Result<PathBuf, io::Error> {
     PathBuf::from(s).canonicalize()
 }
 
+fn parse_cache_time(s: &str) -> color_eyre::Result<u32> {
+    let duration = match parse_duration(s) {
+        Ok(duration) => duration,
+        Err(err) => {
+            if matches!(
+                err,
+                DurationError::UnknownUnit { ref unit, .. }
+                if unit.is_empty()
+            ) {
+                match s.parse::<u32>() {
+                    Ok(seconds) => return Ok(seconds),
+                    Err(err) if *err.kind() == IntErrorKind::PosOverflow => {
+                        return Err(color_eyre::eyre::eyre!("cache time is too large"))
+                    }
+                    Err(_) => {}
+                }
+            }
+            return Err(err.into());
+        }
+    };
+    let secs = duration.as_secs_f64();
+    assert!(
+        matches!(secs.classify(), FpCategory::Normal | FpCategory::Zero),
+        "humantime should not return NaN or infinite values"
+    );
+    if secs != secs.trunc() {
+        return Err(color_eyre::eyre::eyre!(
+            "cache time in seconds cannot be fractional"
+        ));
+    }
+    if secs > u32::MAX as f64 {
+        return Err(color_eyre::eyre::eyre!("cache time is too large"));
+    }
+    Ok(secs as u32)
+}
+
 #[derive(Debug, Parser)]
 #[clap(name = "Zy")]
 #[clap(about, version, setting = AppSettings::DeriveDisplayOrder)]
@@ -41,8 +79,8 @@ pub struct Args {
     #[clap(default_value = ".", parse(try_from_os_str = parse_canonicalize_dir))]
     pub dir: PathBuf,
 
-    /// Sets the address to listen on (repeatable)
-    /// Valid: `3000`, `127.0.0.1`, `127.0.0.1:3000` [default: 127.0.0.1:3000]
+    /// Sets the address to listen on (repeatable) [default: 127.0.0.1:3000]
+    /// Valid: `3000`, `127.0.0.1`, `127.0.0.1:3000`
     #[clap(short, long, value_name = "URI", multiple_occurrences = true)]
     #[clap(verbatim_doc_comment, parse(try_from_str = addr_from_str))]
     pub listen: Vec<SocketAddr>,
@@ -59,8 +97,11 @@ pub struct Args {
     #[clap(long = "404", value_name = "FILE", default_value = "404.html")]
     pub not_found: String,
 
-    /// Cache time (max-age) in seconds
-    #[clap(short, long, value_name = "SECS", default_value_t = 3600)]
+    /// Cache time (max-age) [default: 1M]
+    /// Valid: `10` for 10 seconds, `1h`, `1year 6months`
+    #[clap(short, long, value_name = "SECS", verbatim_doc_comment)]
+    #[clap(default_value = "1M", hide_default_value(true))]
+    #[clap(parse(try_from_str = parse_cache_time))]
     pub cache: u32,
 
     /// Disable Cross-Origin Resource Sharing (CORS)
